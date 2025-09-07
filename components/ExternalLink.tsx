5d78db7e9646588072a099d3bf198a8e614d318a
smasher44 <georgelibatique819@gmail.com>
2025-09-07 22:32:06 +0800

    Refactor ExternalLink component for improved performance and type safety
    - Converted ExternalLink to a memoized component to optimize rendering.
    - Enhanced type annotations by specifying ReactNode for better clarity.
    - Utilized useCallback for event handling, improving performance.
    - Set displayName for the component to aid in debugging.

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,24 +1,30 @@
 import { Href, Link } from 'expo-router';
 import { openBrowserAsync } from 'expo-web-browser';
-import { type ComponentProps } from 'react';
+import { memo, ReactNode, useCallback, type ComponentProps } from 'react';
+
+
 import { Platform } from 'react-native';
 
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href & string };
 
-export function ExternalLink({ href, ...rest }: Props): React.ReactNode {
+export const ExternalLink = memo(({ href, ...rest }: Props): ReactNode => {
+  const handlePress = useCallback(async (event: any) => {
+    if (Platform.OS !== 'web') {
+      // Prevent the default behavior of linking to the default browser on native.
+      event?.preventDefault();
+      // Open the link in an in-app browser.
+      await openBrowserAsync(href);
+    }
+  }, [href]);
+
   return (
     <Link
       target="_blank"
       {...rest}
       href={href}
-      onPress={async (event) => {
-        if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
-          event?.preventDefault();
-          // Open the link in an in-app browser.
-          await openBrowserAsync(href);
-        }
-      }}
+      onPress={handlePress}
     />
   );
-}
+});
+
+ExternalLink.displayName = 'ExternalLink';
